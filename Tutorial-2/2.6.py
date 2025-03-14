8eb0c15f9212b3f6ed5bd628c69d59f1eedfea62
aleena-99 <knp22cs022@ceknpy.ac.in>
2025-03-14 09:56:13 +0530

    Update 2.6.py

--- Tutorial-2/2.6.py
+++ Tutorial-2/2.6.py
@@ -1,9 +1,13 @@
+def is_prime(n):
+    if n < 2: return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
-#Write a program to remove all occurrence of a substring from a string
+def separate_primes_composites(lst):
+    primes = [n for n in lst if is_prime(n)]
+    composites = [n for n in lst if n > 1 and not is_prime(n)]
+    return primes, composites
 
-s= input("Enter the string")
-sub= input("Enter the substring ")
-
-if sub in s:
-    s = s.replace(sub, "")
-    print(s)
+print(separate_primes_composites([2, 4, 5, 9, 11, 15, 17]))
