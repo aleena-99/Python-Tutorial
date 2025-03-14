82c0966ecb2b903bf45446c11d6746110d21e16c
aleena-99 <knp22cs022@ceknpy.ac.in>
2025-03-14 09:48:03 +0530

    Update 2.2.py

--- Tutorial-2/2.2.py
+++ Tutorial-2/2.2.py
@@ -1,11 +1,4 @@
+def decimal_to_binary(n):
+    return bin(n)[2:]
 
-# Write a program to remove characters at odd index positions from a string
-
-
-s= input("Enter the string : ")
-result=[]
-for i in range(len(s)):
-    if i % 2 == 0:
-        result.append(s[i])
-
-print("".join(result))
+print(decimal_to_binary(10))
