92354218cf2dace3bec2b81e2781119f3a5cdeb2
aleena-99 <knp22cs022@ceknpy.ac.in>
2025-03-14 09:55:47 +0530

    Update 2.5.py

--- Tutorial-2/2.5.py
+++ Tutorial-2/2.5.py
@@ -1,19 +1,7 @@
+def separate_types(lst):
+    integers = [x for x in lst if isinstance(x, int)]
+    floats = [x for x in lst if isinstance(x, float)]
+    strings = [x for x in lst if isinstance(x, str)]
+    return integers, floats, strings
 
-"""Write a program to slice the string into two separate strings; one with all the
-characters in the odd indices and one with all characters in even indices.
-"""
-
-
-s= input("Enter the string : ")
-list1=list(s)
-odd=[ ]
-even=[]
-for i in range(len(list1)):
-    if i%2==0:
-        even.append(list1[i])
-    else:
-        odd.append(list1[i])
-
-print(list1)
-print("even elements = ",even)
-print("odd elements = ",odd)
+print(separate_types([1, 2.5, "hello", 3, 4.7, "world"]))
