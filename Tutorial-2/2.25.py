b75e4158a7b59381fbf3b78d7a97ef89d71c064c
aleena-99 <knp22cs022@ceknpy.ac.in>
2025-03-14 09:50:48 +0530

    Update 2.25.py

--- Tutorial-2/2.25.py
+++ Tutorial-2/2.25.py
@@ -1,9 +1,4 @@
+def factorial(n):
+    return 1 if n == 0 else n * factorial(n - 1)
 
-""" Program to remove all duplicate elements from a list"""
-
-def remove_duplicates(lst):
-    return list(set(lst))
-
-n = int(input("Enter the number of elements: "))
-numbers = [int(input(f"Enter number {i+1}: ")) for i in range(n)]
-print(f"\nList after removing duplicates: {remove_duplicates(numbers)}")
+print(factorial(5))
