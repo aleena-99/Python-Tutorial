6280cc66716ac13d19f8a827588cf47a72b53105
aleena-99 <knp22cs022@ceknpy.ac.in>
2025-03-14 09:32:35 +0530

    Update 1.9.py

--- Tutorial-1/1.9.py
+++ Tutorial-1/1.9.py
@@ -1,17 +1,5 @@
-
-def is_prime(n):
-    if n <= 1:
-        return False 
-    
-    for i in range(2, int(n ** 0.5) + 1):  
-        if n % i == 0:
-            return False  
-    
-    return True  
-
-num = int(input("Enter a number: "))
-
-if is_prime(num):
-    print(f"{num} is a prime number.")
-else:
-    print(f"{num} is not a prime number.")
+x, y = map(int, input("Enter X and Y: ").split())
+result = 1
+for _ in range(abs(y)):
+    result *= x
+print(result if y >= 0 else 1/result)
