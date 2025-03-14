c2d44f717d70cddfefcdf57d109f4c6edc25c459
aleena-99 <knp22cs022@ceknpy.ac.in>
2025-03-14 09:21:46 +0530

    Update 1.13.py

--- Tutorial-1/1.13.py
+++ Tutorial-1/1.13.py
@@ -1,17 +1,9 @@
 
-def reverse_number(n):
-    reversed_num = 0
-    
-    while n > 0:
-        digit = n % 10 
-        reversed_num = reversed_num * 10 + digit  
-        n //= 10
-    
-    return reversed_num
+sides = sorted([float(input("Enter side 1: ")), 
+                float(input("Enter side 2: ")), 
+                float(input("Enter side 3: "))])
 
-num = int(input("Enter a number: "))
-
-if num < 0:
-    print(f"Reversed number: -{reverse_number(abs(num))}")
+if sides[0]*2 + sides[1]2 == sides[2]*2:
+    print("Right-angled triangle")
 else:
-    print(f"Reversed number: {reverse_number(num)}")
+    print("Not a right-angled triangle")
