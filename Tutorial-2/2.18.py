2940dfc4304640e8140bfbc8dbc635871f148814
aleena-99 <knp22cs022@ceknpy.ac.in>
2025-03-14 09:47:16 +0530

    Update 2.18.py

--- Tutorial-2/2.18.py
+++ Tutorial-2/2.18.py
@@ -1,16 +1,5 @@
+def reverse_halves(s):
+    mid = len(s) // 2
+    return s[:mid][::-1] + s[mid:][::-1]
 
-""" Write a Python program to print the factorial of a number using recursion"""
-
-def factorial(n):
-    if n == 0 or n == 1:
-        return 1
-    return n * factorial(n - 1)
-
-# Get user input
-num = int(input("Enter a number: "))
-
-# Validate input
-if num < 0:
-    print("Factorial is not defined for negative numbers.")
-else:
-    print(f"Factorial of {num} is {factorial(num)}")
+print(reverse_halves("HelloWorld"))
