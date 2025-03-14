07ba000ff8dce8a78276e9c17d6075936653292f
aleena-99 <knp22cs022@ceknpy.ac.in>
2025-03-14 09:24:36 +0530

    Update 1.17.py

--- Tutorial-1/1.17.py
+++ Tutorial-1/1.17.py
@@ -1,7 +1,2 @@
-
-n = int(input("Enter the value of n: "))
-
-for i in range(1, n + 1):
-    print(f"\nMultiplication Table of {i}:") 
-    for j in range(1, 11):  
-        print(f"{i} x {j} = {i * j}") 
+n = int(input("Enter a number: "))
+print(f"Sum of cubes: {sum(i**3 for i in range(2, n+1, 2))}")
