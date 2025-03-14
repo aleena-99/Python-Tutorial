1d22c6f6d664b588a2f583ee97fafee1d47dde9a
aleena-99 <knp22cs022@ceknpy.ac.in>
2025-03-14 09:44:16 +0530

    Update 2.13.py

--- Tutorial-2/2.13.py
+++ Tutorial-2/2.13.py
@@ -1,18 +1,4 @@
+def split_indices(s):
+    return s[1::2], s[0::2]
 
-""" Write a python function to find the area of a circle."""
-
-
-import math
-
-def area_of_circle(radius):
-    if radius < 0:
-        return "Radius cannot be negative"
-    
-    area = math.pi * radius ** 2
-    return area
-
-
-r = float(input("Enter the radius of the circle: "))
-
-
-print(f"Area of the circle: {area_of_circle(r):.2f}")
+print(split_indices("HelloWorld"))
