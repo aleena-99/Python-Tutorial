a00aaa236a5f7fc364a73707bfaf34c5c95402ac
aleena-99 <knp22cs022@ceknpy.ac.in>
2025-03-14 09:28:30 +0530

    Update 1.3.py

--- Tutorial-1/1.3.py
+++ Tutorial-1/1.3.py
@@ -1,11 +1,5 @@
-
-def OoE(num):
-    if num==0:
-        print(f"{num} is zero.")
-    elif num%2==0:
-        print(f"{num} is even.")
-    else:
-          print(f"{num} is odd.")
-
-num=int(input("Enter the number: "))
-check=OoE(num)
+n = int(input("Enter N: "))
+for i in range(1, n+1):
+    for j in range(1, 11):
+        print(f"{i} × {j} = {i*j}")
+    print()
