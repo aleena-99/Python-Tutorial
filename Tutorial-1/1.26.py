685af53760bcc58a2696b60b624f64225beed74d
aleena-99 <knp22cs022@ceknpy.ac.in>
2025-03-14 09:26:26 +0530

    Update 1.26.py

--- Tutorial-1/1.26.py
+++ Tutorial-1/1.26.py
@@ -1,11 +1,5 @@
 
-import math
-
-x1 = float(input("Enter x1: "))
-y1 = float(input("Enter y1: "))
-x2 = float(input("Enter x2: "))
-y2 = float(input("Enter y2: "))
-
-distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-
-print(f"Distance between ({x1}, {y1}) and ({x2}, {y2}) is {distance:.2f}")
+a, b = 0, 1
+for _ in range(10):
+    print(a, end=" ")
+    a, b = b, a + b
