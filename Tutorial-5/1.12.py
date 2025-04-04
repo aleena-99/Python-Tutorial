e8d63d8c1302b7de11a6d45598aca79f5a4e6566
aleena-99 <knp22cs022@ceknpy.ac.in>
2025-04-04 22:21:24 +0530

    Create 1.12.py

---
+++ Tutorial-5/1.12.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+data = [["Alice", 24], ["Bob", 27], ["Charlie", 22]]
+df = pd.DataFrame(data, columns=["Name", "Age"])
+df.set_index("Name", inplace=True)
+df.to_excel("data.xlsx")
