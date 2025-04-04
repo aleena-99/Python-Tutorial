2fd22a5fe345bc5f3409bba47f729aca2271a4c6
aleena-99 <knp22cs022@ceknpy.ac.in>
2025-04-04 22:22:15 +0530

    Create 1.14.py

---
+++ Tutorial-5/1.14.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+data = {"SN": [1, 2, 3], "Name": ["Linus Torvalds", "Tim Berners-Lee", "Guido van Rossum"],
+        "Country": ["Finland", "England", "Netherlands"], "Contribution": ["Linux Kernel", "World Wide Web", "Python"],
+        "Year": [1991, 1990, 1991]}
+df_contrib = pd.DataFrame(data)
+df_contrib.to_csv("contributions.csv", index=False)
