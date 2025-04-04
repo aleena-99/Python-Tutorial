5b6e44af98c85301103fd9a58ef6af894400f95e
aleena-99 <knp22cs022@ceknpy.ac.in>
2025-04-04 22:15:09 +0530

    Create 1.2.py

---
+++ Tutorial 3/1.2.py
@@ -0,0 +1,12 @@
+x, y = map(float, input("Enter x and y coordinates: ").split())
+
+if x > 0 and y > 0:
+    print("Quadrant I")
+elif x < 0 and y > 0:
+    print("Quadrant II")
+elif x < 0 and y < 0:
+    print("Quadrant III")
+elif x > 0 and y < 0:
+    print("Quadrant IV")
+else:
+    print("Point lies on an axis or at origin")
